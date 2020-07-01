5c662c0996b10e18c18dd15fb79ef04a3fdff449
Evandro Santos <egs.evandro.santos@gmail.com>
2020-07-01 15:36:36 -0300

    User model, migration and spec (test)

---
+++ db/migrate/20200701182831_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :full_name, limit: 100
+      t.string :email, limit: 50
+      t.string :username, limit: 20
+      t.string :password_digest
+
+      t.timestamps
+    end
+    add_column :users, :status, :status
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :status
+  end
+end
