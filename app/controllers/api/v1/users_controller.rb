185d9ce6decf7d2f9e9fe9b62c84b2bef4d5c6ed
Evandro Santos <egs.evandro.santos@gmail.com>
2020-07-02 12:20:02 -0300

    User controller, routes and spec (test)

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+module Api::V1
+  class UsersController < ApplicationController
+    def create
+    end
+  
+    def show
+    end
+  
+    def update
+    end
+  
+    def confirmation
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
+  namespace :api do
+    namespace :v1 do
+      resources :users, except: [:index, :destroy] do
+        patch 'confirmation/:token', action: 'confirmation', on: :collection
+      end
+    end
+  end
 end
