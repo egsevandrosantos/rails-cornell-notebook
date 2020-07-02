f9f31a5704bf73ed3011b4dd3883a471f1e6c5ad
Evandro Santos <egs.evandro.santos@gmail.com>
2020-07-02 11:37:57 -0300

    Validations for User entity

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   enum status: Util.array_to_enum_hash(Status::STATUSES), _prefix: true
   has_secure_password
 
+  validates :full_name, :email, :username, presence: true
+  validates :status, inclusion: { in: Status::STATUSES }
+  validates :full_name, length: { minimum: 3, maximum: 100 }
+  validates :email, length: { minimum: 3, maximum: 50 }
+  validates :username, :password, length: { minimum: 3, maximum: 20 }
+  validates :email, :username, uniqueness: true
+  validates :email, email: true
+
   def status
-    @status ||= Status.new(read_attribute(:status))
+    if !@status.eql?(read_attribute(:status))
+      @status = Status.new(read_attribute(:status))
+    end
+    @status.to_s
   end
 end
--- db/migrate/20200701182831_create_users.rb
+++ db/migrate/20200701182831_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :full_name, limit: 100
-      t.string :email, limit: 50
-      t.string :username, limit: 20
-      t.string :password_digest
+      t.string :full_name, limit: 100, null: false
+      t.string :email, limit: 50, null: false
+      t.string :username, limit: 20, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
-    add_column :users, :status, :status, default: :inactive
+    add_column :users, :status, :status, null: false, default: :inactive
     add_index :users, :email, unique: true
     add_index :users, :username, unique: true
     add_index :users, :status
